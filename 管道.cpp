1582e7c6638bcafa49fc750982405ec63f1952f4
yolo <77111763+yoloeww@users.noreply.github.com>
2023-05-13 19:18:56 +0800

    Create 管道.cpp

---
+++ 管道.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<signal.h>
+
+#define BLOCK_SIGNAL 2
+using namespace std;
+int main() {
+    signala_t block,oblock;
+    // 1.1 初始化
+    sigemptyset(&block);
+    sigemptyset(&oblock);
+
+    sigprocmask(sig)
+    return 0;
+}
